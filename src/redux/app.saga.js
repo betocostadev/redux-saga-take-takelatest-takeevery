2160d56d330c8d9ed13cf99c0f94490834678692
betocostadev <robertomarx@gmail.com>
2019-10-02 19:38:45 -0300

    comments and changes to understand

--- src/redux/app.saga.js
+++ src/redux/app.saga.js
@@ -1,11 +1,37 @@
-import { takeLatest, delay, put } from 'redux-saga/effects';
+// take will fire only once
+// takeEvery will run all the time, like looping the other function it calls
+// takeLatest as the name says, will get only the latest result. It is used like in
+// an async call where you can get more than one result. Like using the delay.
+// delay is like a setTimeout for Sagas
+// import { take, takeEvery, takeLatest, delay, put } from 'redux-saga/effects'
+import { takeLatest, delay, put } from 'redux-saga/effects'
 
+// Now it will delay 2.5 seconds, but even if we click 10 times within these seconds
+// it will add only 1, because it will only... takeLatest!
 export function* onIncrement() {
-  yield console.log('I am incremented');
-  yield delay(3000);
-  yield put({ type: 'INCREMENT_FROM_SAGA' });
+  yield console.log('I am incremented')
+  yield delay(2500)
+  yield put({ type: 'INCREMENT_FROM_SAGA' })
 }
 
 export function* incrementSaga() {
-  yield takeLatest('INCREMENT', onIncrement);
+  yield takeLatest('INCREMENT', onIncrement)
 }
+
+
+/* export function* onIncrement() {
+  yield console.log('I am incremented')
+}
+
+export function* incrementSaga() {
+  // const incrementPayload = yield take('INCREMENT')
+  yield delay(2000)
+  yield takeEvery('INCREMENT', onIncrement)
+  console.log('Increment Saga runs after 2 seconds')
+} */
+
+/* export function* incrementSaga() {
+  // const incrementPayload = yield take('INCREMENT')
+  yield take('INCREMENT')
+  console.log('increment from saga')
+} */
